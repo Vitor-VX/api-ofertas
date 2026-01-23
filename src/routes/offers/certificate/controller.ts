7fe72eb26c923037240e58de607e600bd2e76821
João Victor <joaovictorrocha102019@gmail.com>
2026-01-23 16:54:58 -0300

    Verificando se foi adicionado uppsel

--- src/routes/offers/certificate/controller.ts
+++ src/routes/offers/certificate/controller.ts
@@ -26,7 +26,7 @@ export const createOrder = async (req: Request, res: Response) => {
         );
     }
 
-    if (extras?.length > 0 && !CertificateConfig.extras[extras]) {
+    if (extras && extras.length > 0 && !CertificateConfig.extras[extras]) {
         throw new AppError(
             "UPSELL_NOT_FOUND",
             MSG.EXTRA_NOT_FOUND,

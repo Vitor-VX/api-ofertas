fd2d970d9c7a3c2e475e45472d8388198a4b7d5f
João Victor <joaovictorrocha102019@gmail.com>
2026-01-26 16:41:20 -0300

    Adicionado lógica para enviar dois certificados

--- src/workers/certificate/worker-payments.ts
+++ src/workers/certificate/worker-payments.ts
@@ -6,7 +6,7 @@ import { mercadoPago } from "../../libs/mercadopago";
 import { msg } from "../../utils/logs";
 import { isProd } from "../../utils/isProd";
 import { OrdersCertificate } from "../../database/models/certifcate";
-import { WhatsAppService } from "../../libs/whatsapp";
+import { MediaItem, WhatsAppService } from "../../libs/whatsapp";
 import { generateCertificateImage } from "../../libs/certificate-generate";
 import { BREATHING_BASE64, INPUT_IMAGE_BASE64, INPUT_IMAGE_WITH_PHOTO_BASE64 } from "../../routes/offers/certificate/data";
 
@@ -17,20 +17,30 @@ const connection = {
     password: prod ? "" : process.env.PASSWORD_REDIS_LOCAL
 }
 
-const sendProductToWhataspp = async (number: string, content: string) => {
+const sendProductToWhataspp = async (number: string, files: MediaItem[]) => {
     const whatsapp = new WhatsAppService({
         accessToken: process.env.API_KEY_WHATSAPP ?? "",
         phoneNumberId: process.env.PHONE_ID ?? ""
     });
 
-    whatsapp.sendMedia({
-        to: number,
-        type: "image",
-        caption: "Olá, obrigada pela sua compra. Aqui está seu certificado.",
-        content: content,
-        isBase64: true,
-        fileName: "certificado.png"
-    })
+    if (isProd()) {
+        await whatsapp.sendTemplate({
+            to: number,
+            templateName: "entregar_certificado",
+        });
+
+        await whatsapp.sendMultipleMedia({
+            to: number,
+            type: "image",
+            files
+        });
+    } else {
+        await whatsapp.sendMultipleMedia({
+            to: number,
+            type: "image",
+            files
+        });
+    }
 }
 
 const deliverProduct = async (paymentID: string, id: string) => {
@@ -56,16 +66,53 @@ const deliverProduct = async (paymentID: string, id: string) => {
     const one = names[0] ?? "";
     const two = names[1] ?? "";
 
-    await generateCertificateImage({
-        couple,
-        date: startDate,
-        city,
-        one,
-        two,
-        photo
-    }, BREATHING_BASE64, INPUT_IMAGE_BASE64, INPUT_IMAGE_WITH_PHOTO_BASE64).then((buffer) => {
-        sendProductToWhataspp(order.customer.whatsapp, buffer.toString("base64"));
-    });
+    const [img_not_photo, img_with_photo] = await Promise.all([
+        generateCertificateImage(
+            {
+                couple,
+                date: startDate,
+                city,
+                one,
+                two,
+                photo
+            },
+            BREATHING_BASE64,
+            INPUT_IMAGE_BASE64,
+            null
+        ),
+        generateCertificateImage(
+            {
+                couple,
+                date: startDate,
+                city,
+                one,
+                two,
+                photo
+            },
+            BREATHING_BASE64,
+            INPUT_IMAGE_BASE64,
+            INPUT_IMAGE_WITH_PHOTO_BASE64
+        )
+    ]);
+
+    const withImg = order.certificate.some(el => el.photo !== null);
+    const items: MediaItem[] = [
+        {
+            content: img_not_photo.toString("base64"),
+            fileName: "certificado.png",
+            caption: "✨ Seu certificado do amor",
+        },
+    ];
+
+    if (withImg) {
+        items.push({
+            content: img_with_photo.toString("base64"),
+            fileName: "certificado.png",
+            caption: "✨ Seu certificado do amor",
+        })
+    }
+
+    await sendProductToWhataspp(order.customer.whatsapp, items);
 }
 
 const worker = new Worker("payments-mp", async (job) => {
